42554e3d892ba2c39e11637fc34733c37324a280
Nicolás Salas <nsalas@fastmail.com>
2022-01-10 02:08:15 -0300

    wip: Implement RBT insertion

--- src/rbt.ts
+++ src/rbt.ts
@@ -3,9 +3,10 @@ export enum RedBlackTreeColor {
   Black,
 }
 
-class RedBlackTreeNode {
+export class RedBlackTreeNode {
   public left?: RedBlackTreeNode
   public right?: RedBlackTreeNode
+  public parent?: RedBlackTreeNode
 
   constructor(public color: RedBlackTreeColor, public data: number) {}
 }
@@ -24,11 +25,39 @@ export class RedBlackTree {
       return
     }
 
-    // const newNode = new RedBlackTreeNode(
-    //   RedBlackTreeColor.Red,
-    //   data
-    // )
-    // insert something
+    const newNode = new RedBlackTreeNode(RedBlackTreeColor.Red, data)
+
+    const parent = this.findParentFor(data)
+
+    if (parent === null) {
+      throw new Error('element already exists')
+    }
+
+    if (data < parent.data) {
+      parent.left = newNode
+    } else {
+      parent.right = newNode
+    }
+    newNode.parent = parent
+
+    // now that's inserted, check if coloring is needed
+    const aunt = this.auntForNode(newNode)
+    if (!aunt) {
+      return
+    }
+
+    if (parent.color === RedBlackTreeColor.Red) {
+      if (aunt.color === RedBlackTreeColor.Red) {
+        if (!aunt.parent) {
+          return
+        }
+        this.recolorAround(aunt.parent)
+      } else {
+        // rotate
+      }
+    }
+
+    this.checkRootRecoloring()
   }
 
   public search(query: number) {
@@ -48,5 +77,66 @@ export class RedBlackTree {
 
     // not found
     return null
+  }
+
+  private findParentFor(n: number) {
+    let parent = null
+    let currentNode = this.root
+
+    while (currentNode) {
+      if (currentNode.data === n) {
+        return null
+      }
+
+      parent = currentNode
+      if (n < currentNode.data) {
+        currentNode = currentNode.left
+      } else {
+        currentNode = currentNode.right
+      }
+    }
+
+    // arrived at null
+    return parent
+  }
+
+  private auntForNode(node: RedBlackTreeNode) {
+    const parent = node.parent
+
+    if (!parent) {
+      return null
+    }
+
+    const grandma = parent.parent
+
+    if (!grandma) {
+      return null
+    }
+
+    if (grandma.left?.data === parent.data) {
+      return grandma.right
+    } else {
+      return grandma.left
+    }
+  }
+
+  private recolorAround(node: RedBlackTreeNode) {
+    node.color = RedBlackTreeColor.Red
+
+    if (node.left) {
+      node.left.color = RedBlackTreeColor.Black
+    }
+
+    if (node.right) {
+      node.right.color = RedBlackTreeColor.Black
+    }
+  }
+
+  private checkRootRecoloring() {
+    if (!this.root) {
+      return
+    }
+
+    this.root.color = RedBlackTreeColor.Black
   }
 }
--- tests/rbt.test.ts
+++ tests/rbt.test.ts
@@ -1,10 +1,50 @@
-import { checkRedBlackTree } from './util'
-import { RedBlackTree } from '../src/rbt'
+import { RedBlackTreeChecker } from './util'
 
 describe('A Red Black Tree', () => {
   it('is valid if nothing is inserted', () => {
-    const tree = new RedBlackTree()
+    const checker = new RedBlackTreeChecker()
 
-    expect(checkRedBlackTree(tree)).toEqual(true)
+    expect(() => checker.check()).not.toThrow()
+  })
+
+  it('is valid if only one element is inserted', () => {
+    const checker = new RedBlackTreeChecker()
+
+    checker.insert(1)
+
+    expect(() => checker.check()).not.toThrow()
+  })
+
+  it('is valid if left/right childs are inserted', () => {
+    const checker = new RedBlackTreeChecker()
+
+    checker.insert(5)
+    checker.insert(4)
+    checker.insert(6)
+
+    expect(() => checker.check()).not.toThrow()
+  })
+
+  it('is valid if one recoloring is needed', () => {
+    const checker = new RedBlackTreeChecker()
+
+    checker.insert(3)
+    checker.insert(1)
+    checker.insert(4)
+    checker.insert(5)
+
+    expect(() => checker.check()).not.toThrow()
+  })
+
+  it('is valid if one rotation is needed', () => {
+    const checker = new RedBlackTreeChecker()
+
+    checker.insert(3)
+    checker.insert(1)
+    checker.insert(5)
+    checker.insert(7)
+    checker.insert(6)
+
+    expect(() => checker.check()).not.toThrow()
   })
 })
