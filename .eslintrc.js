434e48094b38fdcb9068b0fa8db2ee1170342593
Nicolás Salas <nsalas@fastmail.com>
2022-01-09 22:49:49 -0300

    Set up eslint

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  rules: {
+    "no-console": "error"
+  }
+}
